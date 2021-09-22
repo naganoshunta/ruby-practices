c1c4d23e66ebd757ac56daa84fc1c2bd1ce6261c
Shunta Nagano <naganoshunta@gmail.com>
2021-09-22 21:32:19 +0900

    カレンダー表示プログラムをメソッドにまとめました
    再利用可能性を高めるために，ただの処理のかたまりだったカレンダープログラムをメソッドにまとめました。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,19 +2,15 @@
 require 'optparse'
 require 'date'
 
-Current_Month = Date.today.month
-Current_Year = Date.today.year
-
-params = {}
 opts = OptionParser.new
-opts.on("-m", "--month MONTH", desc = "表示する月を指定してください") { |month|
-  params[:month] = month.to_i
+opts.on("-m", "--month [MONTH]", desc = "表示する月を指定してください") { |month|
+  month.to_i
 }
-
-opts.on("-y", "--year YEAR", desc = "表示する年を指定してください") { |year|
-  params[:year] = year.to_i
+opts.on("-y", "--year [YEAR]", desc = "表示する年を指定してください") { |year|
+  year.to_i
 }
-opts.parse!(ARGV)
+params = {}
+opts.parse!(ARGV, into: params)
 
 def valid_date?(params)
   if Date.valid_date?(params[:year], params[:month], 1)
@@ -24,36 +20,100 @@ def valid_date?(params)
   end
 end
 
-if valid_date?(params)
-  first_day = Date.new(params[:year], params[:month], 1)
-  last_day = Date.new(params[:year], params[:month], -1)
-else
-  first_day = Date.new(Current_Year, Current_Month, 1)
-  last_day = Date.new(Current_Year, Current_Month, -1)
-end
-
-dates = {}
-first_day.step(last_day, step = 1) { |date|
-  dates[date] = date.strftime('%e')
-}
-
-dates.each do |date, str|
-  dates[date] = "\e[7m#{str}\e[0m" if date == Date.today
-end
-
-dates.each do |date, str|
-  if date.saturday?
-    dates[date] = str + "\n"
+def identify_first_day(params)
+  if valid_date?(params)
+    Date.new(params[:year], params[:month], 1)
   else
-    dates[date] = str + " "
+    Date.new(Date.today.year, Date.today.month, 1)
   end
 end
 
-month_year_title = "#{first_day.month}月 #{first_day.year}年".center(20)
-weeks = ["日", "月", "火", "水", "木", "金", "土"]
-weeks_title = weeks.join(" ")
-calendar_title = month_year_title + "\n" + weeks_title + "\n"
-calendar_blanks = "   " * first_day.wday
+def identify_last_day(params)
+  if valid_date?(params)
+    Date.new(params[:year], params[:month], -1)
+  else
+    Date.new(Date.today.year, Date.today.month, -1)
+    end
+end
 
-calendar = [calendar_title, calendar_blanks, dates.values.join, "\n"]
-print calendar.join
+def identify_year(params)
+  identify_first_day(params).year
+end
+
+def identify_month(params)
+  identify_first_day(params).month
+end
+
+
+def obtain_calendar_info(params)
+  {
+    first_day: identify_first_day(params),
+    last_day: identify_last_day(params),
+    year: identify_year(params),
+    month: identify_month(params),
+    validity: valid_date?(params)
+  }
+end
+
+def obtain_calendar(calendar_info)
+  calendar = {}
+  calendar_info[:first_day].step(calendar_info[:last_day], 1) do |date|
+    calendar[date] = date.strftime("%e")
+  end
+  calendar
+end
+
+def mark_today(calendar)
+  calendar[Date.today] = "\e[7m#{calendar[Date.today]}\e[0m" if calendar.has_key?(Date.today)
+  calendar
+end
+
+def add_newlinechar_and_space(calendar)
+  calendar.each do |date, date_for_display|
+    if date.saturday?
+      calendar[date] = date_for_display + "\n"
+    else
+      calendar[date] = date_for_display + " "
+    end
+  end
+  calendar
+end
+
+def obtain_calendar_for_display(calendar)
+  calendar.values.join
+end
+
+def make_calendar_title(calendar_info)
+  month_year_title = "#{calendar_info[:month]}月 #{calendar_info[:year]}年".center(20)
+  weeks_title = ["日", "月", "火", "水", "木", "金", "土"].join(" ")
+  calendar_title = month_year_title + "\n" + weeks_title + "\n"
+  calendar_title
+end
+
+def make_calendar_head_blanks(calendar_info)
+  calendar_head_blanks = "   " * calendar_info[:first_day].wday
+  calendar_head_blanks
+end
+
+def make_warning_message(calendar_info)
+  if !calendar_info[:validity]
+    "引数が未指定もしくは無効です。今月・今年のカレンダーを表示します。\n"
+  end
+end
+
+def format_calendar_for_display(calendar_info, calendar_for_display)
+  calendar_title = make_calendar_title(calendar_info)
+  calendar_head_blanks = make_calendar_head_blanks(calendar_info)
+  warning_message = make_warning_message(calendar_info)
+  [warning_message, calendar_title, calendar_head_blanks, calendar_for_display].join
+end
+
+calendar_info = obtain_calendar_info(params)
+calendar = obtain_calendar(calendar_info)
+calendar = mark_today(calendar)
+calendar = add_newlinechar_and_space(calendar)
+calendar_for_display = obtain_calendar_for_display(calendar)
+formatted_calendar = format_calendar_for_display(calendar_info, calendar_for_display)
+
+print formatted_calendar
+
