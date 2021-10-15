30afc71441cc66bae4cbe3d10c3eb01da49ef0b9
Shunta Nagano <sht3ng@gmail.com>
2021-10-15 18:13:51 +0900

    bowling.rb内のBowlingScorerクラスの修正に伴ってテストの修正を行いました。

--- 04.bowling/bowling_test.rb
+++ 04.bowling/bowling_test.rb
@@ -4,51 +4,12 @@ require 'minitest/autorun'
 require_relative 'bowling'
 
 class BowlingTest < Minitest::Test
-  def test_score139
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 139
-    assert_equal expected, result
-  end
-
-  def test_score164
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 164
-    assert_equal expected, result
-  end
-
-  def test_score107
-    score = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 107
-    assert_equal expected, result
-  end
-
-  def test_score134
-    score = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 134
-    assert_equal expected, result
-  end
-
-  def test_score300
-    score = 'X,X,X,X,X,X,X,X,X,X,X,X'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 300
-    assert_equal expected, result
-  end
-
-  def test_score0
-    score = '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'
-    scoresheet = Bowling::ScoreSheet.new(score)
-    result = Bowling::Scorer.calculate(scoresheet.frames)
-    expected = 0
-    assert_equal expected, result
+  def test_bowling_scorer
+    assert_equal 139, BowlingScorer.calculate('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 164, BowlingScorer.calculate('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 107, BowlingScorer.calculate('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 134, BowlingScorer.calculate('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 300, BowlingScorer.calculate('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 0, BowlingScorer.calculate('0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
   end
 end
